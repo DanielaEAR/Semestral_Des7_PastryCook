97a2fdc54dec9a6a46af463c19ea1b71c5aaff62
Daniela A <daniela_3102@outlook.com>
2022-11-20 22:01:22 -0500

    Desarrollo del frontend y aplicación de estilos

--- Secciones/header.php
+++ Secciones/header.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <!-- CSS only -->
+    <link href="http://localhost/SemestralFinal/css/estilosSecciones.css" rel="stylesheet" >
+    <link href="http://localhost/SemestralFinal/css/estilosIndex.css" rel="stylesheet" >
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+     <!-- FUENTES TEXT -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <title>Pastry Cook</title>
 </head>
 <body>
-    
-</body>
-</html>
+    <header>
+        <div class="logo">
+            <img class="imgLogo" src="img/logo/logo1.1.png"> 
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -1,12 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
+<?php
+    include("Secciones/header.php");
+?>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="container">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="navbar-brand" id="nav1" aria-current="page" href="#">Inicio 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
+                                <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+                            </svg>
+                        </a> 
+                    </li>
+                    <li class="nav-item">
+                        <a class="navbar-brand" id="nav1" href="#">Acceder 
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <br><br>
+    <div class="bannerInfo">
+        <h1> ¡Comparte tu receta especial! </h1>
+        <h5> En Pastry Cook, podrás compartir y encontrar recetas para hacer tus postres favoritos </h5>
+    </div>
+    <div class="bannerImg">
+        <img class="imgbanner" src="img/banners/bannerImg.png"> 
+    </div>
     
 </body>
 </html>
